72cb285b0193f94edfe0a0418f8eb770d90da85d
Karunya1912 <raveendrankarunya01@gmail.com>
2024-07-15 23:24:49 +0530

    first commit

---
+++ 1.scala
@@ -0,0 +1,26 @@
+object IMS
+{
+    def getProductList(products: List[String] = Nil): List[String] = {
+
+        val input = scala.io.StdIn.readLine("Enter product name (or type 'done' to finish): ")
+        if (input.toLowerCase == "done") products
+        else getProductList(products :+ input)
+    }
+
+    def printproductlist(products:List[String]):Unit={
+        for(i<-products.indices){
+            println(s"${i+1}.${products(i)}")
+        }
+    }
+
+    def total(products:List[String]):Int={
+        products.size
+    }
+
+    def main(args:Array[String]):Unit= {
+        val productlist=getProductList()
+        printproductlist(productlist)
+        println(s"no.of products:${total(productlist)}")
+    }
+    
+}
