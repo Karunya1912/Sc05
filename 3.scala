b4f7276ea5684337a83d7f749b3a94067c1a113c
Karunya1912 <raveendrankarunya01@gmail.com>
2024-07-15 23:40:38 +0530

    commit messag

--- 3.scala
+++ 3.scala
@@ -1,17 +1,18 @@
-import scala.io.StdIn.readLine
-import scala.io.StdIn.readInt
+object Fibonacci {
+  def fibonacci(num: Int): Int = {
+    if (num <= 1)
+      num
+    else
+      fibonacci(num - 1) + fibonacci(num - 2)
+  }
 
-object fabbinocci {
-    def main(args: Array[String]): Unit = {
-        
-        def fabbinocci_series(n:Int): Int = n match {
-            case x if (x==0)=> 0
-            case x if(x ==1) => 1
-            case _ => fabbinocci_series(n-1)+ fabbinocci_series(n-2)
-        }
-        print("Enter the number to fabbinnoci: ")
-        var a = readInt()
-        println(s"The fibonacci of ${a} is : ${fabbinocci_series(a)}")
-
+  def main(args: Array[String]): Unit = {
+    print("Enter the number of terms: ")
+    val n = scala.io.StdIn.readInt()
+    println(s"Fibonacci series up to $n terms:")
+    for (i <- 0 until n) {
+      print(s"${fibonacci(i)} ")
     }
+    println()
+  }
 }
